0b8edde93eb8b92658f35e9eec929b0280017240
okv <okvokvspb@hotmail.com>
2023-05-09 16:01:40 +0300

    09.05.2023 #15

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,108 @@
+import BaseApi from './BaseApi.js';
+//параметры подключения: 
+const baseUrl = 'https://mesto.nomoreparties.co/v1/cohort-59';
+const headers = {
+	Authorization: 'ba4cd59f-b9e5-45d4-a657-2ca4f8fa9389',
+	'Content-Type': 'application/json'
+};
+class Api extends BaseApi {
+	constructor(baseUrl, headers) {
+		super(baseUrl)
+		this._baseUrl = baseUrl;
+		this._headers = headers;
+	};
+
+	//метод получения данных пользователя
+	_getUserData() {
+		return this._request('users/me', {
+			method: 'GET',
+			headers: this._headers,
+			// credentials: 'include', отсылать куки при расположении b/f частей на разных доменах
+		})
+	};
+
+	//метод получения массива карточек с сервера
+	getAllCardsData() {
+		return this._request('cards', {
+			method: 'GET',
+			headers: this._headers,
+			// credentials: 'include',
+		})
+	};
+
+	//метод получения начальных данных (общий)
+	getInitialData() {
+		return Promise.all([this._getUserData(), this.getAllCardsData()])
+	};
+
+	//метод замены аватара пользователя	
+	setAvatar(_link) {
+		return this._request('users/me/avatar', {
+			method: 'PATCH',
+			headers: this._headers,
+			// credentials: 'include',
+			body: JSON.stringify({
+				avatar: _link
+			})
+		})
+	};
+
+	//метод сохранения данных пользователя в профиль на сервер
+	setUserInfo(_name, _description) {
+		return this._request('users/me', {
+			method: 'PATCH',
+			headers: this._headers,
+			// credentials: 'include',
+			body: JSON.stringify({
+				name: _name,
+				about: _description
+			})
+		})
+	};
+
+	//метод добавления новой карточки на сервер и получения информации о результате
+	addNewCard(_cardName, _cardLink) {
+		return this._request('cards', {
+			method: 'POST',
+			headers: this._headers,
+			// credentials: 'include',
+			body: JSON.stringify({
+				name: _cardName,
+				link: _cardLink
+			})
+		})
+	};
+
+	//метод удаления своей карточки на сервере и получения информации о результате
+	deleteCard(_cardId) {
+		return this._request(`cards/${_cardId}`, {
+			method: 'DELETE',
+			headers: this._headers,
+			// credentials: 'include',
+		})
+	};
+
+	//метод установки лайка и получения данных о результате
+	setLike(_cardId) {
+		return this._request(`cards/${_cardId}/likes`, {
+			method: 'PUT',
+			headers: this._headers,
+			// credentials: 'include',
+		})
+	};
+
+	//метод удаления лайка (поставленного пользователем)
+	deleteLike(_cardId) {
+		return this._request(`cards/${_cardId}/likes`, {
+			method: 'DELETE',
+			headers: this._headers,
+			// credentials: 'include',
+		})
+	};
+
+};
+
+//инициализация класса запросов к серверу
+const api = new Api(baseUrl, headers);
+
+export default api;
---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,53 @@
+import BaseApi from './BaseApi.js';
+//параметры подключения: 
+const baseUrl = 'https://auth.nomoreparties.co';
+const headers = { 'Content-Type': 'application/json' };
+class Auth extends BaseApi {
+	constructor(baseUrl, headers) {
+		super(baseUrl)
+		this._baseUrl = baseUrl;
+		this._headers = headers;
+	};
+
+	//метод регистрации пользователя
+	registrate(_email, _password) {
+		return this._request('signup', {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				email: _email,
+				password: _password
+			})
+		})
+	};
+
+	//метод авторизации зарегистрированного пользователя
+	logIn(_email, _password) {
+		return this._request('signin', {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify({
+				email: _email,
+				password: _password
+			})
+		})
+	};
+
+	//метод аутентификации(отправки жетона для проверки)
+	checkToken(_token) {
+		return this._request('users/me', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${_token}`,
+				// credentials: 'include',
+			}
+		})
+	};
+
+};
+
+//инициализация класса авторизации
+const auth = new Auth(baseUrl, headers);
+
+export default auth;
